a109d172235c79dce3466bc4a5e5b6e58fe3a5b3
Gary Alway <garydouglasalway@gmail.com>
2023-09-11 22:48:45 +0100

    refactor truncate table

--- src/client.ts
+++ src/client.ts
@@ -1,5 +1,4 @@
 import { DynamoDB } from '@aws-sdk/client-dynamodb'
-import { AttributeMap } from './types'
 
 export const TABLE_NAME = 'demo-table'
 export const REGION = 'eu-west-1'
@@ -8,26 +7,3 @@ export const dynamoClient = new DynamoDB({
   region: REGION,
   endpoint: process.env.IS_OFFLINE ? 'http://localhost:8000' : undefined
 })
-
-const getItemKeyAndValue = (item: AttributeMap, key?: string) =>
-  key ? { [`${key}`]: item[`${key}`] } : {}
-
-export const truncateTable = async (
-  client: DynamoDB,
-  TableName: string,
-  hash: string,
-  range?: string
-): Promise<void> => {
-  const { Items } = await client.scan({ TableName })
-  if (!Items) {
-    return
-  }
-  const keys = Items.map((item: AttributeMap) => ({
-    ...getItemKeyAndValue(item, hash),
-    ...getItemKeyAndValue(item, range)
-  }))
-  if (!keys.length) {
-    return
-  }
-  await Promise.all(keys?.map(Key => client.deleteItem({ TableName, Key })))
-}
--- src/truncate-table.ts
+++ src/truncate-table.ts
@@ -1,9 +1,26 @@
-import { TABLE_NAME, dynamoClient, truncateTable } from './client'
+import { TABLE_NAME, dynamoClient } from './client'
+import { AttributeMap } from './types'
+
+const getItemKeyAndValue = (item: AttributeMap, key?: string) =>
+  key ? { [`${key}`]: item[`${key}`] } : {}
 
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
 Promise.resolve()
   .then(async () => {
-    await truncateTable(dynamoClient, TABLE_NAME, 'pk', 'sk')
+    const { Items } = await dynamoClient.scan({ TableName: TABLE_NAME })
+    if (!Items) {
+      return
+    }
+    const keys = Items.map((item: AttributeMap) => ({
+      ...getItemKeyAndValue(item, 'pk'),
+      ...getItemKeyAndValue(item, 'sk')
+    }))
+    if (!keys.length) {
+      return
+    }
+    await Promise.all(
+      keys?.map(Key => dynamoClient.deleteItem({ TableName: TABLE_NAME, Key }))
+    )
   })
   .catch(err => {
     console.error(err)
